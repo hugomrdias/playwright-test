a3d7fdd8ff44e4dffe2e751e7eb42eaa7e3da30e
Hugo Dias <hugomrdias@gmail.com>
2020-02-13 17:22:01 +0000

    chore: change mocks folder name

--- test.js
+++ test.js
@@ -4,11 +4,5 @@ const test = require('ava');
 const m = require('.');
 
 test('title', (t) => {
-    const err = t.throws(() => {
-        m(123);
-    }, TypeError);
-
-    t.is(err.message, 'Expected a string, got number');
-
     t.is(m('unicorns'), 'unicorns & rainbows');
 });
