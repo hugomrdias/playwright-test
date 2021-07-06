63b8c2e517c63796f587e361bb70e138935a929f
Hugo Dias <hugomrdias@gmail.com>
2021-07-06 13:02:47 +0100

    fix: update minor deps

--- mocks/test1.zora.js
+++ mocks/test1.zora.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { test } = require('zora')
+const { good, bad } = require('./lib')
 
 test('a first sub test', (t) => {
   t.ok(true)
@@ -16,4 +17,9 @@ test('a first sub test', (t) => {
   t.test('inside', (t) => {
     t.ok(false, 'oh no!')
   })
+})
+
+test('external', async (t) => {
+  t.equal(await good(), 'good')
+  t.equal(await bad(), 'bad')
 })
