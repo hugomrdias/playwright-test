268543178e105ac027b20f5156ca6d2bb9f07dd9
Hugo Dias <hugomrdias@gmail.com>
2023-05-12 12:33:02 +0100

    feat: add process.exit (#542)
    * feat: add process.exit
    
    Allows ending and failing run with process.exit(0) or process.exit(233).

--- mocks/test.mocha.js
+++ mocks/test.mocha.js
@@ -7,6 +7,7 @@ const { good, bad } = require('./lib')
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
+      console.log(process)
       is([1, 2, 3].indexOf(4), -1)
     })
 
--- src/utils/inject-process.js
+++ src/utils/inject-process.js
@@ -1,3 +1,17 @@
 // @ts-nocheck
-export { default as process } from 'process/browser'
+// eslint-disable-next-line unicorn/prefer-module
+const _process = require('process/browser')
+
+const p = {
+  ..._process,
+  exit: (code = 0) => {
+    if (code === 0) {
+      globalThis.PW_TEST.end(false)
+    } else {
+      globalThis.PW_TEST.end(true)
+    }
+  },
+}
+
+export const process = p
 // https://github.com/ionic-team/rollup-plugin-node-polyfills
